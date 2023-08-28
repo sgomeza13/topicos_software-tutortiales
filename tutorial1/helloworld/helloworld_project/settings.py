5004386b068ca387ee19a1ef464aa671ec70be0b
sgomeza13 <sgomeza13@eafit.edu.co>
2023-08-28 17:43:17 -0500

    tutorial 3a-3b

--- tutorial1/helloworld/helloworld_project/settings.py
+++ tutorial1/helloworld/helloworld_project/settings.py
@@ -11,10 +11,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from pathlib import Path
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -76,13 +82,8 @@ WSGI_APPLICATION = 'helloworld_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+IMAGE_STORAGE_CLASS = 'pages.utils.ImageLocalStorage'
--- tutorial1/helloworld/pages/urls.py
+++ tutorial1/helloworld/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView
 
+from .views import HomePageView, AboutPageView, ContactPageView, ImageViewFactory, ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView, ImageViewNoDI
+from .utils import ImageLocalStorage
 urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
@@ -8,5 +9,13 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
-    path('products/created', ProductCreateView.as_view(), name='created')
+    path('products/created', ProductCreateView.as_view(), name='created'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+    path('image/save', ImageViewNoDI.as_view(), name='imagenodi_save'),
+
 ]
